52852a64bc5ad0d5083b3cfb184059ec9130a976
Carlos Afonso <fonsi@google.com>
2023-01-04 14:24:58 +0000

    Add execution permissions to delete_resources.sh and improve it

--- delete_resources.sh
+++ delete_resources.sh
@@ -2,6 +2,21 @@
 
 set -euo pipefail
 
+# Load customizable environment variables
+source envs.sh
+
+# Define derived variables
+export VPC_FULLY_QUALIFIED_NETWORK_NAME="projects/$GCP_PROJECT_NUMBER/global/networks/$VPC_NETWORK_NAME"
+export NETWORK_PEERING_RANGE_NAME="vertex-matching-engine"
+export BUCKET_URI="gs://${DATA_BUCKET_NAME}"
+export DELTA_LOCATION="${BUCKET_URI}/delta/"
+export INDEX_NAME="vertex-matching-engine-index"
+export INDEX_ENDPOINT_NAME="vertex-maching-engine-index"
+export DEPLOYED_INDEX_ID="mydeployedidx"
+
+export INDEX_ID=$(gcloud ai indexes list --region $GCP_REGION --filter="displayName:$INDEX_NAME" --format="value(name)")
+export INDEX_ENDPOINT_ID=$(gcloud ai index-endpoints list --region $GCP_REGION --filter="displayName:$INDEX_ENDPOINT_NAME" --format="value(name)")
+
 gcloud --quiet ai index-endpoints undeploy-index $INDEX_ENDPOINT_ID  --deployed-index-id $DEPLOYED_INDEX_ID
 echo "Index undeployed from endpoint"
 
